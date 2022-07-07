6ba2af6b0ecb700c589336eeb7e70eba50cec79f
anujbansal19 <99503944+anujbansal19@users.noreply.github.com>
2022-07-07 16:54:36 +0530

    Add files via upload

---
+++ z_analysis_conf.py
@@ -0,0 +1,8 @@
+SSID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
+AUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # you have to fill the SID and token of your own.
+FROM_NUMBER = '+13202887820'
+TO_NUMBER = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # fill your own phone number
+API_KEY = 'This is your Bolt Cloud account API key'
+DEVICE_ID = 'This is the ID of your Bolt device'
+FRAME_SIZE = 10
+MUL_FACTOR = 3
